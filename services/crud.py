a31e4a4ed00d7d667ba9193e2f4ed3e94fcea793
Scott Roriston <scott@systemfuel.com>
2022-11-03 00:21:14 +0100

    more readable function name

--- services/crud.py
+++ services/crud.py
@@ -2,7 +2,7 @@ from sqlalchemy.orm import Session
 from url_shortener import models, schemas, utils
 
 
-def get_source_url_by_key(database: Session, short_url_key: str) -> models.URL:
+def get_url_data_by_key(database: Session, short_url_key: str) -> models.URL:
     ''''Get source URL via short URL key lookup'''
     return (
         database.query(models.URL)
@@ -15,7 +15,7 @@ def create_short_url(database: Session, source_url: schemas.SourceURL):
     ''''Create a database entry creating a short URL key'''
     short_url_key = utils.generate_short_url_key()
 
-    while get_source_url_by_key(database, short_url_key):
+    while get_url_data_by_key(database, short_url_key):
         short_url_key = utils.generate_short_url_key()
 
     database_url = models.URL(
